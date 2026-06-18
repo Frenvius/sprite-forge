1a0091e7e6d3c73bb47eecc1d49e858e02fa34fb
Frenvius <frenvius@gmail.com>
2026-06-18 12:42:04 -0300

    fix: disable contextmenu in find window

--- src/find.tsx
+++ src/find.tsx
@@ -3,4 +3,10 @@ import { createRoot } from 'react-dom/client';
 import FindApp from './FindApp';
 import './index.css';
 
+if (typeof window !== 'undefined') {
+	document.addEventListener('contextmenu', (e) => {
+		e.preventDefault();
+	});
+}
+
 createRoot(document.getElementById('root')!).render(<FindApp />);
